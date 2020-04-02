e5a71db73d54cc16a1c1bbd826fe8575812fc828
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-02 00:26:12 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS.push(@brand)
-    end 
+    !(BRANDS.include?(@brand))? BRANDS.push(@brand):false
   end
   
   BRANDS = []
