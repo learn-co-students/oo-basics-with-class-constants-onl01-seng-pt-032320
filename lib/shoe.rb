69185c55b0a6b3693b478f5d12090d41954feaef
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-02 00:17:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS.push(@brand)
+    end 
   end
+  
+  BRANDS = []
+  
+  
 
   def cobble
     self.condition = "new"
